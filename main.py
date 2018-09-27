918729085b8b87e2bb2021857d87e5e37ad06b0a
Ryan Diel <rdiel01@gmail.com>
2018-09-26 20:42:58 -0500

    added @app.route for /signup-verification

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True   # display runtime errors in the browser
 
-@app.route('/')
+@app.route('/', methods="")
 def base():
     return render_template("signup.html")
+
+@app.route('/signup-verification', methods='POST')
+def signup_verification():
+    
+
+
 
 app.run()
