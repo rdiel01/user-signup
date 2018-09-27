aa13f605139f8b7775871c90e7bf19b761731cd9
Ryan Diel <rdiel01@gmail.com>
2018-09-26 19:52:32 -0500

    added imports, app.Flask, app.DEBUG and first @app.route(/)

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True   # display runtime errors in the browser
+
+@app.route('/')
+def base():
+    return render_template("login.html")
+
+app.run()
