ebe5b4013f7ef48f49abea3878bb545bfaf3046f
Ryan Diel <rdiel01@gmail.com>
2018-09-28 22:20:27 -0500

    added value handlers and request.form variables for verification fuction

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True   # display runtime errors in the browser
+app.config["DEBUG"] = True   # display runtime errors in the browser
 
-@app.route('/', methods="")
+username = ""
+password = ""
+email = ""
+
+@app.route("/", methods=["GET"])
 def base():
-    return render_template("signup.html")
+    return render_template("signup.html", html_username=username, html_email=email)
 
-@app.route('/signup-verification', methods='POST')
-def signup_verification():
-    
-
+@app.route("/verification", methods=["POST"])
+def verification():
+    username = request.form['typed_username']
+    password = request.form['typed_password']
+    email = request.form['typed_email']
+    pass
 
 
 app.run()
