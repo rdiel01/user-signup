0f7cccfc823e128524aef4f0e77a0962d1d5442b
Ryan Diel <rdiel01@gmail.com>
2018-09-29 21:33:17 -0500

    working on correct error returns

--- main.py
+++ main.py
@@ -4,15 +4,7 @@ app = Flask(__name__)
 
 app.config["DEBUG"] = True   # display runtime errors in the browser
 
-username = ""
-password = ""
-email = ""
-invalid=[' ','/','\\','\'','"','@']
-username_error = False
-password_error = False
-email_error = False
-
-def error_check(string):
+def error(string):
     """
     checks is a string has any spaces in it and if it is within 3-20 chars long
     ```
@@ -27,11 +19,11 @@ def password_verification(pass_1,pass_2):
     """
     checks if password value matches verify password value
     ```
-    pass1, a string
-    pass2, a string
+    pass_1, a string
+    pass_2, a string
     """
-    if pass1 == pass2:
-        return True
+    if pass_1 == pass_2:
+        return error(pass_1)
     else:
         return False
 
@@ -42,13 +34,13 @@ def verify_email(email):
     email, a string
     """
     if '@' in email and '.' in email:
-        return error_check(email)
+        return error(email)
     else:
         return True
 
 @app.route("/", methods=["GET"])
 def base():
-    return render_template("signup.html", html_username=username, html_email=email)
+    return render_template("signup.html")
 
 @app.route("/verification", methods=["POST"])
 def verification():
@@ -59,23 +51,30 @@ def verification():
     password, must be 3-20 char long with no spaces
     email, **optional, but must have a @ and no spaces
     """ 
-    username_error = error_check(request.form['typed_username'])
-    if username_error == False:
-        username = request.form['typed_username']
+    username = ""
+    password = ""
+    email = ""
+    username_error = False
+    password_error = False
+    email_error = False
 
-    password_error = error_check(request.form['typed_password'])
-    if password_error == False and password_verification(request.form['typed_password'],request.form['verify_password']):
+    if not error(request.form['typed_username']):
+        username = request.form['typed_username']
+    else:
+        username_error = True
+
+    if not password_verification(request.form['typed_password'],request.form['verify_password']):
         password = request.form['typed_password']
 
     if request.form['typed_email']:
-        email_error = verify_email(request.for['typed_email'])
-    if email_error == False:
-        email = request.form['typed_email']
+        email_error = verify_email(request.form['typed_email'])
+        if not email_error:
+          email = request.form['typed_email']
 
     if not username_error and not password_error and not email_error:
         return render_template("welcome.html",html_username=username)
     else:
-        return render_template("signup.html", html_username=username, html_email=email)
+        return render_template("signup.html",html_username=username,html_email=email,html_username_error=username_error,html_password_error=password_error,html_password_verification_error=password_verification(request.form['typed_password'],request.form['verify_password']),html_email_error=email_error)
 
 
 app.run()
